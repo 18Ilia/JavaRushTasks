2fe34b115d57db0f9ccbced6c79f61b71b00cdcb
Ilya <ily12@inbox.ru>
2024-04-29 14:37:21 +0300

    Add solution javaSyntax task0404

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0404/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task04.task0404;
+
+/* 
+Заполненный прямоугольник
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int column = 0;
+        while (column < 5){
+            int string = 0;
+            while (string < 10){
+                System.out.print("Q");
+                string++;
+            }
+            System.out.println();
+            column++;
+        }
+
+    }
+}
