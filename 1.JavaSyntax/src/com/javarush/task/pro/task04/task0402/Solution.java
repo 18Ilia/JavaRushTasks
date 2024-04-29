52d902bf2b66295f72950fdaa4c08df2257cafd0
Ilya <ily12@inbox.ru>
2024-04-29 13:01:18 +0300

    Add solution javaSyntax task0402

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0402/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task04.task0402;
+
+import java.util.Scanner;
+
+/* 
+Все любят Мамбу
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String text = " любит меня.";
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        int x = 1;
+        while (x <= 10){
+            System.out.println(name + text);
+            x++;
+        }
+    }
+}
