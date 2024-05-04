7f264cbf9dd42f88343efcb4011a208fe0a4ab8f
Ilya <ily12@inbox.ru>
2024-05-04 23:03:01 +0300

    Add solution javaSyntax task0411 не решена

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0411/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task04.task0411;
+
+/* 
+Четные числа
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        for (int x = 0; x <= 15; x++){
+            System.out.println(x);
+        }
+
+    }
+}
