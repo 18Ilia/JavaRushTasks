bef1f9b24c69b95e77bcea4cb4435d5ba0c81592
Ilya <ily12@inbox.ru>
2024-05-10 22:24:04 +0300

    Add solution javaSyntax task0414

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0414/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task04.task0414;
+
+import java.util.Scanner;
+
+/* 
+Хорошего не бывает много
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        int number = scanner.nextInt();
+        do {
+            System.out.println(s);
+            number--;
+        }while (0 < number && number < 4);
+    }
+}
